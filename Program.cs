a8c7a88f569ed135677fce8bb9b1bc5d2ca1eddc
Ryan McCaffery <16667079+mccaffers@users.noreply.github.com>
2021-11-11 20:42:59 +0000

    Initial scan of folder for .csv files

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace backtesting_engine
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("Local feeder starting... ");
+
+            for (var i = 0; i < args.Length; i++)
+            {
+                DirectoryInfo di = new DirectoryInfo(args[i]);
+                var files = di.GetFiles("*.csv").OrderBy(x => x.Name);
+
+                foreach (var file in files) {
+                    System.Console.WriteLine(file.Name);
+                }
+            }
+
+        }
+    }
+}
