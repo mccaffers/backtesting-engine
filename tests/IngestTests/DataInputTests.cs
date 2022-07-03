8c0777cef79bbc89a1a036012b1b671b58d4b41f
Ryan McCaffery <16667079+mccaffers@users.noreply.github.com>
2022-07-03 15:09:49 +0100

    Fixing a unit test

--- tests/IngestTests/DataInputTests.cs
+++ tests/IngestTests/DataInputTests.cs
@@ -146,7 +146,7 @@ public class DataInputTests
         consumerMock.Setup<Task>(x=>x.ConsumeAsync(It.IsAny<BufferBlock<PriceObj>>(), It.IsAny<CancellationToken>()))
                         .Returns(Task.FromResult(0));
 
-        var taskManagerMock = new Mock<TaskManager>(consumerMock.Object, ingestMock.Object, reportingMock.Object, envMock.Object); // can't mock program
+        var taskManagerMock = new Mock<TaskManager>(consumerMock.Object, ingestMock.Object); // can't mock program
 
         ingestMock.Object.fileNames.Add(Path.Combine(PathUtil.GetTestPath("TestEnvironmentSetup"), "testSymbol.csv"));
 
