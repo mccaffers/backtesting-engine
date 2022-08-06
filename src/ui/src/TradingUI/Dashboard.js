eb66cd9969cbd842b063c0c151eeafa6b40ffa9e
Ryan McCaffery <16667079+mccaffers@users.noreply.github.com>
2022-08-06 23:25:26 +0100

    Switched to canvasjs

--- src/ui/src/TradingUI/Dashboard.js
+++ src/ui/src/TradingUI/Dashboard.js
@@ -6,7 +6,11 @@ import TradeWindow from './TradeWindow/TradeWindow';
 import TradeInput from './TradeInput/TradeInput';
 import TradeUpdate from './TradeUpdate';
 
-import Chart from "react-apexcharts";
+import CanvasJSReact from '../libs/canvasjs.react';
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+// import Chart from "react-apexcharts";
 const signalRMsgPack = require("@microsoft/signalr-protocol-msgpack");
 
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0; 
@@ -17,39 +21,29 @@ const Chat = () => {
     var max= new Date("2020-01-05T22:04:12.821Z").getTime();
     var range = max - min
 
- 
+    
     const [ series, setSeries ] = useState(
         {
-            "width": 1000,
-            "series": [{
-                "name": "series-1",
-                "data": [
-                ]}
-            ],
-            "options": {
-                chart: {
-                    animations: {
-                        enabled:false
-                    }
-                  },
-                  title: {
-                    text: '',
-                    align: 'left'
-                  },
-                  xaxis: {
-                    type: 'datetime',
-                  },
-                  yaxis: {
-                    tooltip: {
-                      enabled: true
-                    }
-                },
-                candlestick: {
-                    wick: {
-                      useFillColor: true,
-                    }
-                  }
-            }
+            animationEnabled: true,
+            theme: "light2", // "light1", "light2", "dark1", "dark2"
+            exportEnabled: true,
+            title: {
+                text: "Netflix Stock Price in 2016"
+            },
+            subtitles: [{
+                text: "Weekly Averages"
+            }],
+            axisX: {
+            },
+            axisY: {
+                prefix: "$",
+                title: "Price"
+            },
+            data: [{				
+                type: "candlestick",
+                xValueType: "dateTime",
+                dataPoints: []
+            }]
         }
   
     );
@@ -66,97 +60,73 @@ const Chat = () => {
         // Chart.exec("trading",'updateSeries', series);
         // console.log(series);
         // Chart.exec("trading",'updateSeries', series);
+        console.log(JSON.stringify(series));
+
     });
 
-    const indexValue = useRef(0);
+    
+    // const indexValue = useRef(0);
     const timeValue = useRef(0);
+
+    var chartRef = useRef();
 
     function UpdateChart(OHLCObj){
         
-        setSeries((prevState) => {
+        // setSeries((prevState) => {
 
-            let newState = prevState;
-            let newArray = prevState.series[0];
-            const eventDate = new Date(OHLCObj.d).getTime();
+            // let newState = prevState;
+            const eventDate = +new Date(OHLCObj.d);
 
-            // Track current index to prevent a findIndex search on every tick
-            if(indexValue.current === 0){
-                indexValue.current = newArray.data.findIndex((obj => obj.x===eventDate));
-            }
+            // // Track current index to prevent a findIndex search on every tick
+            // if(indexValue.current === 0){
+            //     indexValue.current = dataPoints.findIndex((obj => obj.x.data===eventDate));
+            // }
 
-            // Save the current date
-            if(timeValue.current === 0){
-                timeValue.current = eventDate;
-            }
+            // // Save the current date
+            // if(timeValue.current === 0){
+            //     timeValue.current = eventDate;
+            // }
 
-            // If the date has changed lets update the current one and refresh the index
-            if(timeValue.current!==eventDate){
-                timeValue.current = eventDate;
-                indexValue.current = newArray.data.findIndex((obj => obj.x===eventDate));
-            }
+            // // If the date has changed lets update the current one and refresh the index
+            // if(timeValue.current!==eventDate){
+            //     timeValue.current = eventDate;
+            //     indexValue.current = dataPoints.findIndex((obj => obj.x===eventDate));
+            // }
 
             // const index = newArray.data.findIndex((obj => obj.x===eventDate));
             
-            // -1 means it doesn't exist, lets start a new element
-            let priceEvent = [ OHLCObj.o, OHLCObj.h, OHLCObj.l, OHLCObj.c];
-            if(indexValue.current==-1){
-                const keepAmount = 80;
-                if(newArray.data.length>keepAmount){
-                    newArray.data = newArray.data.slice(newArray.data.length-keepAmount);
+            setSeries((previousState) => {
+                let previousDataPoints = previousState.data[0].dataPoints;
+
+                let indexValue = previousDataPoints.findIndex((obj => obj.x===eventDate));
+                // -1 means it doesn't exist, lets start a new element
+                let priceEvent = [ OHLCObj.o, OHLCObj.h, OHLCObj.l, OHLCObj.c];
+                if(indexValue==-1){
+                    const keepAmount = 80;
+                    if(previousDataPoints.length>keepAmount){
+                        previousDataPoints = previousDataPoints.slice(previousDataPoints.length-keepAmount);
+                    }
+                    previousDataPoints = [...previousDataPoints, { y: priceEvent, x: eventDate} ];
+                    indexValue = previousDataPoints.length-1;
+                } else {
+                    // dataPoints.current[indexValue.current].x = dataPoints.current[indexValue.current].x;
+                    previousDataPoints[indexValue].y = priceEvent;
                 }
-                newArray.data = [...newArray.data, { x: eventDate, y: priceEvent} ];
-                indexValue.current = newArray.data.length-1;
-            } else {
-                newArray.data[indexValue.current].x = newArray.data[indexValue.current].x;
-                newArray.data[indexValue.current].y = priceEvent;
-            }
+                previousState.data[0].dataPoints = previousDataPoints;
 
-            newArray.name="candlestick";
-            newArray.type="candlestick";
-            // if(newArray.width === 1000){
-            //     newArray.width = 999;
-            // } else {
-            //     newArray.width = 1000;
-            // }
+                return previousState;
+            });
 
-            let high=0
-            let low=Number.MAX_SAFE_INTEGER;
+            chartRef.current.render();
+
         
-            let minDate = null;
-            let range = null;
-            let max = eventDate;
-            
-            minDate = new Date(newArray.data[0].x).getTime();
-            range = max-minDate;
 
-            newArray.data.forEach(function(item){
-                if(item.y[1] > high){
-                    high = item.y[1];
-                }
-                if(item.y[2] < low){
-                    low = item.y[2];
-                }
-            })
-
-            newState.options = {
-                ...newState.options,
-                xaxis: {
-                    min: minDate,
-                    max: eventDate,
-                    range: range
-                },
-                yaxis: {
-                    min: low,
-                    max: high+(Math.random()/10000000),
-                }
-            };
             
-            newState.series[0] = newArray;
-            
-            return newState;
+            // return newState;
 
-        });
+        
     }
+
     function AddTrade(OHLCObj){
 
         
@@ -211,7 +181,7 @@ const Chat = () => {
                         setAccount(message.Content)
                     } else if(message.Activity == "trade"){
                         var OHLCObj = JSON.parse(message.Content);
-                        AddTrade(OHLCObj);
+                        // AddTrade(OHLCObj);
                     }
 
                     // Chart.exec('trading', "updateSeries");
@@ -254,11 +224,17 @@ const Chat = () => {
             <hr />
             {/* <ChatWindow chat={chat}/> */}
             <div>Account: {account}</div>
-            <Chart
+            {/* <Chart
               id="trading"
               options={series.options}
               series={series.series}
               width={series.width}
+            /> */}
+
+            <CanvasJSChart options = {series}
+            onRef={(ref) => {
+                chartRef.current = ref;
+              }}
             />
         </div>
     );
