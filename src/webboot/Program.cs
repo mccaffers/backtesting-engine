7c950eb42b987f9e21315dfe3daf41398ae21833
Ryan McCaffery <16667079+mccaffers@users.noreply.github.com>
2022-08-13 23:01:46 +0100

    Tidying up, and removing unused projects

--- src/webboot/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Task.Run(async () => {
-                await backtesting_engine.Program.Main(new string[1]{"web"});
-            }).Wait();
-        }
-    }
