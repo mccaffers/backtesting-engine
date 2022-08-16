0fb3e35c8b367deec4b57ee42a80064db6ddc9f2
Ryan McCaffery <16667079+mccaffers@users.noreply.github.com>
2022-08-16 07:24:06 +0100

    Ignoring a security sensitive alert when using a random generator for a string, with no security application

--- src/utilities/DictionaryKeyStrings.cs
+++ src/utilities/DictionaryKeyStrings.cs
@@ -1,7 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Utilities;
 
 public static class DictionaryKeyStrings
 {
+    [SuppressMessage("Sonar Code Smell", "S2245:Using pseudorandom number generators (PRNGs) is security-sensitive", Justification = "Random function has no security use")]
     public static string OpenTrade(string symbol, DateTime date){
         var randomInt = new Random().Next(200); 
         return symbol + "-" + date + "-" + randomInt;
